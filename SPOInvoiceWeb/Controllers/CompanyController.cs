ba03ba6c02e797c51c1165e0bda47b85f55a2690
Stefan Holmberg <stefan.holmberg@systementor.se>
2019-01-08 13:38:09 +0100

    Edit

--- SPOInvoiceWeb/Controllers/CompanyController.cs
+++ SPOInvoiceWeb/Controllers/CompanyController.cs
@@ -28,5 +28,60 @@ namespace SPOInvoiceWeb.Controllers
             }
             return View(viewModel);
         }
+
+        [HttpGet]
+        public ActionResult Edit(int id)
+        {
+            using (var entities = new SPOInvoiceEntities())
+            {
+                var model = entities.Company.FirstOrDefault(c => c.id == id);
+                var viewModel = new ViewModels.CompanyEditViewModel();
+                viewModel.id = model.id;
+                viewModel.InvoiceAddress = model.InvoiceAddress;
+                viewModel.InvoiceDefaultPaymentTermsDays = 
+                    model.InvoiceDefaultPaymentTermsDays.HasValue ? model.InvoiceDefaultPaymentTermsDays.Value
+                    : 30;
+                viewModel.InvoiceEmail = model.InvoiceEmail;
+                viewModel.InvoiceName = model.InvoiceName;
+                viewModel.InvoicePostAddress = model.InvoicePostAddress;
+                viewModel.InvoicePostNo = model.InvoicePostNo;
+                viewModel.name = model.name;
+                viewModel.OrgNo = model.OrgNo;
+                viewModel.Founded = model.Founded.HasValue ? model.Founded.Value : DateTime.Now;
+                return View(viewModel);
+            }
+
+        }
+
+
+
+        [HttpPost]
+        public ActionResult Edit(ViewModels.CompanyEditViewModel viewModel)
+        {
+            if(!ModelState.IsValid)
+            {
+                return View(viewModel);
+            }
+            using (var entities = new SPOInvoiceEntities())
+            {
+                var model = entities.Company.FirstOrDefault(c => c.id == viewModel.id);
+
+                model.InvoiceAddress = viewModel.InvoiceAddress;
+                model.InvoiceDefaultPaymentTermsDays = viewModel.InvoiceDefaultPaymentTermsDays;
+                model.InvoiceEmail = viewModel.InvoiceEmail;
+                model.InvoiceName = viewModel.InvoiceName;
+                model.InvoicePostAddress = viewModel.InvoicePostAddress;
+                model.InvoicePostNo = viewModel.InvoicePostNo;
+                model.name = viewModel.name;
+                model.OrgNo = viewModel.OrgNo;
+                model.Founded = viewModel.Founded;
+
+                entities.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+        }
+
+
     }
 }
---
+++ SPOInvoiceWeb/ViewModels/CompanyEditViewModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SPOInvoiceWeb.ViewModels
+{
+    public class CompanyEditViewModel
+    {
+        public int id { get; set; }
+        public string OrgNo { get; set; }
+        public string name { get; set; }
+        public string InvoiceName { get; set; }
+        public string InvoiceAddress { get; set; }
+        public string InvoicePostNo { get; set; }
+        public string InvoicePostAddress { get; set; }
+        public string InvoiceEmail { get; set; }
+        public int InvoiceDefaultPaymentTermsDays { get; set; }
+        public System.DateTime Founded { get; set; }
+        public bool StorKund { get; set; }
+
+    }
+}
