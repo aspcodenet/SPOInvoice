3faa9a128f1fe89ac223bd4d91a7e4bce1d1d53b
Stefan Holmberg <stefan.holmberg@systementor.se>
2019-01-08 11:53:18 +0100

    Invoicing

---
+++ SPOInvoiceWeb/Controllers/CompanyController.cs
@@ -0,0 +1,32 @@
+﻿using SPOInvoiceWeb.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SPOInvoiceWeb.Controllers
+{
+    public class CompanyController : Controller
+    {
+        // GET: Company
+        public ActionResult Index()
+        {
+            var viewModel = new ViewModels.CompanyListViewModel();
+            using (var entities = new SPOInvoiceEntities())
+            {
+                foreach(var dbCompany in entities.Company)
+                {
+                    viewModel.Companies.Add( new ViewModels.CompanyListViewModel.CompanyViewModel
+                    {
+                        Id = dbCompany.id,
+                        Name = dbCompany.name,
+                        OrgNo = dbCompany.OrgNo
+                    }
+                        );
+                }
+            }
+            return View(viewModel);
+        }
+    }
+}
---
+++ SPOInvoiceWeb/ViewModels/CompanyListViewModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SPOInvoiceWeb.ViewModels
+{
+    public class CompanyListViewModel
+    {
+        public CompanyListViewModel()
+        {
+            Companies = new List<CompanyViewModel>();
+        }
+        public class CompanyViewModel
+        {
+            public int Id { get; set; }
+            public string Name { get; set; }
+            public string OrgNo { get; set; }
+        }
+        public List<CompanyViewModel> Companies { get; set; }
+    }
+}
