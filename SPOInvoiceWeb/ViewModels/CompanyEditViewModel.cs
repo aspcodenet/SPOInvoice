40e280b0d8a561e2b865014e05f3d68aaea99c1b
Stefan Holmberg <stefan.holmberg@systementor.se>
2019-01-09 08:54:34 +0100

    some validation

--- SPOInvoiceWeb/ViewModels/CompanyEditViewModel.cs
+++ SPOInvoiceWeb/ViewModels/CompanyEditViewModel.cs
@@ -9,16 +9,32 @@ namespace SPOInvoiceWeb.ViewModels
     public class CompanyEditViewModel
     {
         public int id { get; set; }
+
+        [StringLength(15)]
         public string OrgNo { get; set; }
+
+        [StringLength(255)]
         public string name { get; set; }
+
         [StringLength(255)]
         public string InvoiceName { get; set; }
+
+        [StringLength(255)]
         public string InvoiceAddress { get; set; }
+
+        [StringLength(255)]
         public string InvoicePostNo { get; set; }
+
         [Required]
         public string InvoicePostAddress { get; set; }
+
+        [EmailAddress]
+        [Required]
         public string InvoiceEmail { get; set; }
+
         public int InvoiceDefaultPaymentTermsDays { get; set; }
+
+        [DataType(DataType.Date)]
         public System.DateTime Founded { get; set; }
         public bool StorKund { get; set; }
 
